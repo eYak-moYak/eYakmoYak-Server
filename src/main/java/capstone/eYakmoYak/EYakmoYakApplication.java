9227a00dc470512f84304665a0aa8d1d50bb8d70
otndus <xuyeon219@gmail.com>
2024-04-16 00:42:40 +0900

    build: 프로젝트 세팅

---
+++ src/main/java/capstone/eYakmoYak/EYakmoYakApplication.java
@@ -0,0 +1,13 @@
+package capstone.eYakmoYak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EYakmoYakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EYakmoYakApplication.class, args);
+	}
+
+}
