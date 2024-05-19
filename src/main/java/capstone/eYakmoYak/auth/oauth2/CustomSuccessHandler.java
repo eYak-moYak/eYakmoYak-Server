11153124bd0a9c869c2e54f4aa24f9c46a27abfc
otndus <xuyeon219@gmail.com>
2024-05-19 21:59:02 +0900

    update: Handler 수정

--- src/main/java/capstone/eYakmoYak/auth/oauth2/CustomSuccessHandler.java
+++ src/main/java/capstone/eYakmoYak/auth/oauth2/CustomSuccessHandler.java
@@ -1,5 +1,4 @@
 package capstone.eYakmoYak.auth.oauth2;
-
 import capstone.eYakmoYak.auth.dto.CustomOAuth2User;
 import capstone.eYakmoYak.auth.jwt.JWTUtil;
 import capstone.eYakmoYak.auth.util.LoginUtil;
@@ -11,38 +10,31 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
 @Component
 @RequiredArgsConstructor
 public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
-
     private final JWTUtil jwtUtil;
     private final LoginUtil utils;
-
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-
+        // 유저 정보
+        //OAuth2User
         CustomOAuth2User customUserDetails = (CustomOAuth2User) authentication.getPrincipal();
         String username = customUserDetails.getUsername();
-
+        // 토큰 생성
         String access = jwtUtil.createJwt("access", username, 600000L);
         String refresh = jwtUtil.createJwt("refresh", username, 86400000L);
-
+        //Refresh 토큰 저장
         utils.addRefreshEntity(username, refresh, 86400000L);
-
-        Map<String, String> tokens = new HashMap<>();
-        tokens.put("access", access);
-        tokens.put("refresh", refresh);
-
+        response.setHeader("access", access);
+        response.addCookie(utils.createCookie("refresh", refresh, 60*60*60*60, "/"));
         response.setStatus(HttpStatus.OK.value());
-        response.setContentType("application/json");
-        new ObjectMapper().writeValue(response.getWriter(), tokens);
 
-        getRedirectStrategy().sendRedirect(request, response, "http://localhost:3000/redirect");
+//        System.out.println("refresh = " + refresh);
+//        System.out.println("access = " + access);
+
     }
+
+
 }
