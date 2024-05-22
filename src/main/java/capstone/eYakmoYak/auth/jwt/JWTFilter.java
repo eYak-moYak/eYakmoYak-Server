9fd23182d038b76c5e035f794784497a8ad467c6
otndus <xuyeon219@gmail.com>
2024-05-22 20:04:19 +0900

    update: access토큰 확인

--- src/main/java/capstone/eYakmoYak/auth/jwt/JWTFilter.java
+++ src/main/java/capstone/eYakmoYak/auth/jwt/JWTFilter.java
@@ -42,6 +42,7 @@ public class JWTFilter extends OncePerRequestFilter {
         }
 
         String accessToken = authorization.split(" ")[1];
+        System.out.println("accessToken = " + accessToken);
 
         // 토큰 만료 여부 확인, 만료시 다음 필터로 넘기지 않음
         try {
