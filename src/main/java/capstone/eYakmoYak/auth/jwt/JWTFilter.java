289b8b76c8dcea77adcbc25433df3b8a5a1984f4
otndus <xuyeon219@gmail.com>
2024-05-22 19:17:53 +0900

    update: 불필요한 코드 제거

--- src/main/java/capstone/eYakmoYak/auth/jwt/JWTFilter.java
+++ src/main/java/capstone/eYakmoYak/auth/jwt/JWTFilter.java
@@ -30,14 +30,6 @@ public class JWTFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        // 헤더 값 출력
-        System.out.println("Request Headers:");
-        Enumeration<String> headerNames = request.getHeaderNames();
-        while (headerNames.hasMoreElements()) {
-            String headerName = headerNames.nextElement();
-            System.out.println(headerName + ": " + request.getHeader(headerName));
-        }
-
         final String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
 
         if (authorization == null || !authorization.startsWith("Bearer ")) {
@@ -50,7 +42,6 @@ public class JWTFilter extends OncePerRequestFilter {
         }
 
         String accessToken = authorization.split(" ")[1];
-        System.out.println("Access Token: " + accessToken);
 
         // 토큰 만료 여부 확인, 만료시 다음 필터로 넘기지 않음
         try {
